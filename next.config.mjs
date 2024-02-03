3d95afad05791435c3530d0c22c96fe047b920fc
Marcin <mgarski@tutanota.com>
2024-02-03 12:15:38 -0500

    Setup project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'media-cdn.tripadvisor.com',
+            },
+        ],
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: [{ loader: '@svgr/webpack', options: { icon: true } }],
+        });
+
+        return config;
+    },
+};
 
 export default nextConfig;
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<main className='m-auto overflow-hidden'>
+			<h1>Hello, Next.js!</h1>
+		</main>
+	);
+}
